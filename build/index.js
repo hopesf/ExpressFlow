a64b1b49b3486cc61cbe1d61bec73dcffdefeb89
hopesf <gecin18@gmail.com>
2023-10-13 16:29:20 +0300

    🔧 updated project

--- build/index.js
+++ build/index.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("selam");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config({ path: `.env.${process.env.NODE_ENV}` });
+const express_1 = __importDefault(require("express"));
+const compression_1 = __importDefault(require("compression"));
+const app = (0, express_1.default)();
+app.use((0, compression_1.default)());
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("selam");
+import dotenv from "dotenv";
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+import express from "express";
+import compression from "compression";
+
+const app = express();
+
+app.use(compression());
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
