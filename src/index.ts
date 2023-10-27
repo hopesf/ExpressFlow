b5fb6ba4e25ae2523ccb70e6c64ceeb2aa51c8d7
hopesf <gecin18@gmail.com>
2023-10-27 17:47:42 +0300

    🔧 updated project

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ connectDb();
 
 // other imports
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+import cors from "cors";
 import compression from "compression";
 import helmet from "helmet";
 
@@ -17,6 +21,32 @@ import routes from "./routes";
 
 // app started
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  //   transports: ["websocket"],
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+let isFirstConnection = true;
+
+io.on("connection", (socket) => {
+  console.log("Bir kullanıcı ana sunucuya bağlandı", socket.id);
+
+  socket.on("mesaj", (data: any) => {
+    console.log("frontdan gelen mesaj:", data);
+  });
+
+  // Server1'den Server2'ye bağlantı kur
+  // const server2Socket = require("socket.io-client")("http://localhost:2002");
+
+  // server2Socket.on("cevap", (data: any) => {
+  //   console.log("Server2'den gelen cevap:", data);
+  // });
+  //   socket.emit("cevapdon", "xasdasdasdad");
+});
 
 // app usages
 app.disable("x-powered-by");
@@ -24,11 +54,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 app.use(express.json());
 app.use(helmet());
+app.use(cors());
 app.use(authMiddleware);
 app.use(routes);
 
 // app listen
 const { PORT, NODE_ENV } = process.env;
-app.listen(PORT, () => console.log(`[${NODE_ENV}] Gate açıldı ${PORT}`));
+httpServer.listen(PORT, () => console.log(`[${NODE_ENV}] Gate açıldı ${PORT}`));
 
 export default app;
