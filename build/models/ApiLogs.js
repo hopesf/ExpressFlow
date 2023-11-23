c71aa845bd4c73a061ce9a880961ec743be9d19c
Debian <bitnami@ip-172-26-6-235.eu-central-1.compute.internal>
2023-11-23 12:02:14 +0000

    removed build

--- build/models/ApiLogs.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const apiLogsSchema = new mongoose_1.Schema({ modelCode: String, merchant: String, responseStatus: String, type: String, description: String }, { versionKey: false, collection: "ApiRobots" });
-const ApiLogs = (0, mongoose_1.model)("ApiLogs", apiLogsSchema, "ApiLogs");
-exports.default = ApiLogs;
